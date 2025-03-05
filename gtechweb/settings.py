11236a91bb9f4efb15b03d9e5b80460fdced4cc3
Amilton Junio <amiltonsogm@gmail.com>
2025-03-05 11:35:10 -0300

    criado novo app para lidar com produtos

--- gtechweb/settings.py
+++ gtechweb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gtechwebs',  # Adicionando a aplicação criada
+    'gtechwebs',
+    'gtech_produtos',   # Adicionando a aplicação criada
 ]
 
 MIDDLEWARE = [
